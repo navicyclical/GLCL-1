6a9f70d83d08d7afa475bdc3a3a6521caa2e32dc
navicyclical <nfthach@gmail.com>
2018-08-22 00:15:45 -0700

    added JS files and styled the tracker page

---
+++ tracker.js
@@ -0,0 +1,13 @@
+let tracker = () => {
+    let plus5 = document.getElementById('plus-5');
+    let counter = document.getElementById('counter');
+    let totalTime = 0;
+
+    let min5 = document.getElementById('5-min');
+    let counter = document.getElementById('counter');
+    let i = 0;
+    min5.addEventListener('click', ()=>{
+      i += 5;
+      counter.innerHTML = i;
+    });
+    console.log(min5);
