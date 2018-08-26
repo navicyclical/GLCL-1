9d38e80137c2b015ac6a4b383b50f3d31531665c
navicyclical <nfthach@gmail.com>
2018-08-26 13:19:00 -0700

    still fixing broken stuff:

--- tracker.js
+++ tracker.js
@@ -1,6 +1,4 @@
 let tracker = () => {
-<<<<<<< HEAD
-
 	let min5 = document.getElementById('5-min')
 	let counter = document.getElementById('counter');
 	let i = 0;
@@ -12,7 +10,6 @@ let tracker = () => {
 };
 
 document.addEventListener('DOMContentLoaded', tracker, false);
-=======
     let plus5 = document.getElementById('plus-5');
     let counter = document.getElementById('counter');
     let totalTime = 0;
@@ -25,4 +22,3 @@ document.addEventListener('DOMContentLoaded', tracker, false);
       counter.innerHTML = i;
     });
     console.log(min5);
->>>>>>> 6a9f70d83d08d7afa475bdc3a3a6521caa2e32dc
