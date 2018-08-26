c2bb2e0534ca7fb2977025d259e28b3904f5ec15
navicyclical <nfthach@gmail.com>
2018-08-26 13:42:01 -0700

    fixed the tracker for 5 minute increments

--- tracker.js
+++ tracker.js
@@ -1,5 +1,13 @@
 let tracker = () => {
-	let min5 = document.getElementById('5-min')
+	let plus5 = document.getElementById('plus-5')
+	let counter = document.getElementById('counter');
+	let i = 0;
+	min5.addEventListener('click', ()=>{
+		i += 5;
+		counter.innerHTML = i;
+	});
+
+	let minus5 = document.getElementById('minus-5')
 	let counter = document.getElementById('counter');
 	let i = 0;
 	min5.addEventListener('click', ()=>{
