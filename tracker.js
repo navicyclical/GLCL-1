11708b93d209fdffa6c959d24a2fb5ac65212e1e
navicyclical <nfthach@gmail.com>
2018-08-27 22:42:28 -0700

    still trying to get the 5 minute counter working

--- tracker.js
+++ tracker.js
@@ -2,13 +2,13 @@ let tracker = () => {
 	let plus5 = document.getElementById('plus-5')
 	let counter = document.getElementById('counter');
 	let totalTime = 0;
-	min5.addEventListener('click', ()=>{
+	plus5.addEventListener('click', ()=>{
 		totalTime += 5;
 		counter.innerHTML = counter;
 	});
 
 	let minus5 = document.getElementById('minus-5')
-	min5.addEventListener('click', ()=>{
+	minus5.addEventListener('click', ()=>{
 		totalTime -= 5;
 		counter.innerHTML = counter;
 	});
